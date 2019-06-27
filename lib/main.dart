c601f66b18de5ed2249e08260870181688c69fa9
drealtunde <tunde.areola@gmail.com>
2019-06-27 08:34:12 +0100

    change in dice on press

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Image(
-          width: 150,
-          height: 150,
-          image: AssetImage('images/dice1.png'),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Image(
-          width: 150,
-          height: 150,
-          image: AssetImage('images/dice2.png'),
-        )
-      ],
+//    leftDiceNumber = 4;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 2;
+                  print("Dice $leftDiceNumber rolled");
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice2.png'),
+              onPressed: () {
+                print("Dice 2 rolled");
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
